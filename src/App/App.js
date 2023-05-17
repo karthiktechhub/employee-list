d3ea5044200b757cdf36808c0ffd22f34650a053
Karthik Khoday <58616458+karthiktechhub@users.noreply.github.com>
2023-05-17 07:29:33 +0000

    removed sidemenu

--- src/App/App.js
+++ src/App/App.js
@@ -37,7 +37,7 @@ function App() {
   const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
-      {/* <SideMenu /> */}
+
       <div className={classes.appMain}>
         <Header />
         <Employees />
